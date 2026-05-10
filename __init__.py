2adb0e51d9185551b7513537cf2ecd2743c62798
balkunovlevnikolaevich-a11y <balkunov.lev.nikolaevich@gmail.com>
2026-05-10 20:56:07 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from .config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
+    from .blueprints.auth import auth_bp
+    from .blueprints.tasks import tasks_bp
+    from .blueprints.api import api_bp
+    from .blueprints.main import main_bp
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(tasks_bp, url_prefix='/tasks')
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
