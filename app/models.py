e6608a27b2dbde5950cddd24a7ecf1dc2728d432
balkunovlevnikolaevich-a11y <balkunov.lev.nikolaevich@gmail.com>
2026-05-10 21:03:11 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,46 @@
+from flask_login import UserMixin
+from . import db
+from datetime import datetime
+import enum
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class UserRole(enum.Enum):
+    CUSTOMER = "Заказчик"
+    EXECUTOR = "Исполнитель"
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.Enum(UserRole), nullable=False)
+    balance = db.Column(db.Integer, default=1000)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    tasks_created = db.relationship('Task', foreign_keys='Task.customer_id', backref='customer', lazy=True)
+    tasks_taken = db.relationship('Task', foreign_keys='Task.executor_id', backref='executor', lazy=True)
+
+class TaskStatus(enum.Enum):
+    OPEN = "Открыта"
+    TAKEN = "Принята"
+    COMPLETED = "Выполнена"
+    VERIFIED = "Подтверждена"
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100), nullable=True)
+    reward = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.Enum(TaskStatus), default=TaskStatus.OPEN)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    file_path = db.Column(db.String(300), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
