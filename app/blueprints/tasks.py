c35e06448bfe56b0daf05b317de48eed0837b149
balkunovlevnikolaevich-a11y <balkunov.lev.nikolaevich@gmail.com>
2026-05-11 00:11:57 +0300

    Update tasks.py

--- app/blueprints/tasks.py
+++ app/blueprints/tasks.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 import os
 from .. import db
-from ..models import Task, TaskStatus, User, UserRole   # ← исправленная строка
+from ..models import Task, TaskStatus, User, Message     # ← исправленная строка
 from ..forms import TaskForm
 from ..utils import generate_ai_description
 
