b657a060f49df8c5eed4c8b86f5983550626dc41
balkunovlevnikolaevich-a11y <balkunov.lev.nikolaevich@gmail.com>
2026-05-10 21:04:49 +0300

    Create main.py

---
+++ app/blueprints/main.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.dashboard'))
+    return render_template('index.html')
+
+@main_bp.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
