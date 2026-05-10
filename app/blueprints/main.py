57df87238977b7163c37a0767d90704f340c1b3a
balkunovlevnikolaevich-a11y <balkunov.lev.nikolaevich@gmail.com>
2026-05-10 23:53:38 +0300

    Update main.py

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 main_bp = Blueprint('main', __name__)
 
@@ -8,8 +8,6 @@ def index():
     if current_user.is_authenticated:
         return redirect(url_for('main.dashboard'))
     return render_template('index.html')
-
-from flask_login import login_required
 
 @main_bp.route('/dashboard')
 @login_required
